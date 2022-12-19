c186b571c26dec92eb036731dd77d20f8c75e7f1
Maura <113387525+maura16@users.noreply.github.com>
2022-12-19 16:48:56 -0700

    Committing changes to 02-bash read me and scripts

--- 01-core/02-bash/01-trino-populate-catalog/solution/trino_populate.sh
+++ 01-core/02-bash/01-trino-populate-catalog/solution/trino_populate.sh
@@ -7,7 +7,6 @@ for schema in 0{01..05}; do
   echo "CREATE SCHEMA memory.schema_$schema;" >> $FILE; 
   for table in 0{01..09} {10..10}; do 
     echo "CREATE TABLE memory.schema_$schema.table_$table as select 1 x;" >> $FILE; 
-    echo "CREATE TABLE memory.schema_$schema.table_$table as select 1 x;"
   done;
 done;
 
